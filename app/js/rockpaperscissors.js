2c50dc69cb245ab30d91afe04105b72e9b453355
nickstoneman <nick.stoneman@gmail.com>
2015-04-14 17:08:01 +0100

    Added logic to function getWinner

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -41,6 +41,14 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    if playerMove > computerMove {
+        winner = playerMove
+    } else {
+        winner = computerMove
+    } else if {
+        winner = tie
+    }
+
     return winner;
 }
 
